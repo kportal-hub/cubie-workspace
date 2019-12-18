b3881eafdc0adb4a398cb761f5112b256ee00969
kportal-hub <59002469+kportal-hub@users.noreply.github.com>
2019-12-18 11:25:30 +0800

    Initial commit

---
+++ .github/workflows/onPushAddCubeWs.js
@@ -0,0 +1,33 @@
+const fs = require('fs');
+const axios = require("axios");
+
+let addCube = async (username, cube) => {
+    // call /add-cube for server
+    try {
+        let res1 = await axios.post("https://cubie.now.sh/api/add-cube", {
+            username,
+            cube
+        });
+        if (res1.data.result) {
+            return (await axios.post("https://cubie.now.sh/api/add-cube-init", {
+                username,
+                cube
+            })).data
+        }
+    } catch (err) {
+        return {
+            result: false,
+            error: err.message
+        }
+    }
+}
+
+const wsOnPush = async () => {
+    const cube = JSON.parse(fs.readFileSync(process.env.NODE_CUBE, 'utf8')).commits[0].message.split(".")[0];
+    const userInfo = JSON.parse(fs.readFileSync(`.cubie/cube.json`, 'utf8')).user;
+    return await addCube(userInfo.username, cube)
+}
+
+wsOnPush().then((res) => {
+    console.log(res)
+})
---
+++ .github/workflows/onPushBuildCubeWS.js
@@ -0,0 +1,35 @@
+const axios = require("axios");
+const fs = require('fs');
+
+let buildCube = async (username, cube, lessons) => {
+    try {
+        let res1 = await axios.post("https://cubie.now.sh/api/build-cube", {
+            username,
+            cube
+        });
+        if (res1.data.result) {
+            return (await axios.post("https://cubie.now.sh/api/build-cube-init", {
+                username,
+                cube,
+                lessons
+            })).data
+        }
+    } catch (err) {
+        return {
+            result: false,
+            error: err.message
+        }
+    }
+
+}
+
+const wsOnPush = async () => {
+    const cube = JSON.parse(fs.readFileSync(process.env.cube, 'utf8')).commits[0].message.split(".")[0];
+    const userInfo = JSON.parse(fs.readFileSync(`.cubie/cube.json`, 'utf8')).user;
+    const lessons = JSON.parse(fs.readFileSync(`builds/${cube}.cube.json`, 'utf8')).lessons;
+    return await buildCube(userInfo.username, cube, lessons)
+}
+
+wsOnPush().then((res) => {
+    console.log(res)
+})
