6dd1ca3d99b03f22025ce35f9a96247223de4ed0
kportal-hub <59002469+kportal-hub@users.noreply.github.com>
2019-12-19 20:09:40 +0800

    Update onPushBuildCubeWS.js

--- .github/workflows/onPushBuildCubeWS.js
+++ .github/workflows/onPushBuildCubeWS.js
@@ -3,12 +3,12 @@ const fs = require('fs');
 
 let buildCube = async (username, cube, lessons) => {
     try {
-        let res1 = await axios.post("https://d9d2270c.ngrok.io/api/build-cube", {
+        let res1 = await axios.post("https://cubie.now.sh/api/build-cube", {
             username,
             cube
         });
         if (res1.data.result) {
-            return (await axios.post("https://d9d2270c.ngrok.io/api/build-cube-init", {
+            return (await axios.post("https://cubie.now.sh/api/build-cube-init", {
                 username,
                 cube,
                 lessons
