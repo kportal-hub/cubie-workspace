6ea4c86f63af41fd977a9ac13c05cd96115bb0e5
kportal-hub <59002469+kportal-hub@users.noreply.github.com>
2019-12-25 18:20:08 +0800

    Update onPushAddCubeWs.js

--- .github/workflows/onPushAddCubeWs.js
+++ .github/workflows/onPushAddCubeWs.js
@@ -1,33 +1,85 @@
-const fs = require('fs');
-const axios = require("axios");
+const axios = require("axios")
+const shell = require("shelljs")
+const crypto = require('crypto');
 
-let addCube = async (username, cube, gitToken) => {
-    // call /add-cube for server
+async function encryptAndPutAuthFile(username, repo, algorithm, gitToken, requestType, silent) {
     try {
+        var cipher = crypto.createCipher(algorithm, gitToken);
+        var encryptedPhrase = cipher.update(requestType, 'utf8', 'hex');
+        encryptedPhrase += cipher.final('hex');
+        shell.exec(`git checkout master`, {silent});
+        shell.exec(`echo ${encryptedPhrase} > ${requestType}.req`, {silent});
+        shell.exec(`git add ${requestType}.req`, {silent});
+        shell.exec(`git commit -m 'add request file'`, {silent});
+        shell.exec(`git push https://${username}:${gitToken}@github.com/${repo} master`, {silent});
+        return true
+    } catch (err) {
+        throw err
+    }
+}
+
+async function removeAuthFiles(username, repo, gitToken, silent) {
+    try {
+        shell.exec(`git checkout master`, {silent});
+        shell.exec(`rm add-cube.req`, {silent});
+        shell.exec(`rm add-cube-init.req`, {silent});
+        shell.exec(`git add add-cube.req`, {silent});
+        shell.exec(`git add add-cube-init.req`, {silent});
+        shell.exec(`git commit -m 'remove request files'`, {silent});
+        shell.exec(`git push https://${username}:${gitToken}@github.com/${repo} master`, {silent});
+        return true
+    } catch (err) {
+        throw err
+    }
+}
+
+// actions on push
+let addCube = async (username, cube, gitToken, repo) => {
+    const algorithm = 'aes256';
+    const _silent = false;
+
+    try {
+        // create add cube request type file
+        await encryptAndPutAuthFile(username, repo.split('/')[1], algorithm, gitToken, "add-cube", _silent);
+
         let res1 = await axios.post("https://cubie.now.sh/api/add-cube", {
             username,
-            cube
+            cube,
+            gitToken,
+            repo: repo.split('/')[1]
         });
         if (res1.data.result) {
-            return (await axios.post("https://cubie.now.sh/api/add-cube-init", {
+            // create add cube init request type file
+            await encryptAndPutAuthFile(username, repo,split('/')[1], algorithm, gitToken, "add-cube-init", _silent);
+
+            let r =(await axios.post("https://cubie.now.sh/api/add-cube-init", {
                 username,
-                cube
-            })).data
+                cube, 
+                gitToken,
+                repo: repo.split('/')[1]
+            })).data;
+            
+            if (r.result) {
+                await removeAuthFiles(username, repo.split('/')[1], gitToken, _silent)
+            }
+            
+            return r;
         }
     } catch (err) {
         return {
             result: false,
-            error: err.message
+            error: "Couldn't add cube: " + err.message
         }
     }
+
 }
 
-const wsOnPush = async (gitToken) => {
+const wsOnPush = async (gitToken, repo) => {
     const cube = JSON.parse(fs.readFileSync(process.env.NODE_CUBE, 'utf8')).commits[0].message.split(".")[0];
     const userInfo = JSON.parse(fs.readFileSync(`.cubie/cube.json`, 'utf8')).user;
-    return await addCube(userInfo.username, cube, gitToken)
+    return await addCube(userInfo.username, cube, gitToken, repo)
 }
 
-wsOnPush(process.argv[2]).then((res) => {
+wsOnPush(process.argv[2], process.argv[2]).then((res) => {
     console.log(res)
 })
