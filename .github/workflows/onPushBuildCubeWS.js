3619d81e3845ba5110b8ee0d9a46be5d2960acf6
kportal-hub <59002469+kportal-hub@users.noreply.github.com>
2019-12-25 17:54:09 +0800

    Update onPushBuildCubeWS.js

--- .github/workflows/onPushBuildCubeWS.js
+++ .github/workflows/onPushBuildCubeWS.js
@@ -1,7 +1,7 @@
 const axios = require("axios");
 const fs = require('fs');
 
-let buildCube = async (username, cube, lessons) => {
+let buildCube = async (username, cube, lessons, gitToken) => {
     try {
         let res1 = await axios.post("https://cubie.now.sh/api/build-cube", {
             username,
@@ -23,13 +23,13 @@ let buildCube = async (username, cube, lessons) => {
 
 }
 
-const wsOnPush = async () => {
+const wsOnPush = async (gitToken) => {
     const cube = JSON.parse(fs.readFileSync(process.env.cube, 'utf8')).commits[0].message.split(".")[0];
     const userInfo = JSON.parse(fs.readFileSync(`.cubie/cube.json`, 'utf8')).user;
     const lessons = JSON.parse(fs.readFileSync(`builds/${cube}.cube.json`, 'utf8')).lessons;
-    return await buildCube(userInfo.username, cube, lessons)
+    return await buildCube(userInfo.username, cube, lessons, gitToken)
 }
 
-wsOnPush().then((res) => {
+wsOnPush(process.argv[2]).then((res) => {
     console.log(res)
 })
