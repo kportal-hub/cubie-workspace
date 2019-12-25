31cf42383b835dfb4993484894eba72188a6624d
ntohidi <nasrin@kidocode.com>
2019-12-25 19:15:32 +0800

    update

--- .github/workflows/onPushAddCubeWs.js
+++ .github/workflows/onPushAddCubeWs.js
@@ -1,18 +1,26 @@
 const fs = require('fs');
 const axios = require("axios");
-const shell = require("shelljs");
 const crypto = require('crypto');
+const Octokit = require("@octokit/rest");
 
 async function encryptAndPutAuthFile(username, repo, algorithm, gitToken, requestType, silent) {
     try {
         var cipher = crypto.createCipher(algorithm, gitToken);
         var encryptedPhrase = cipher.update(requestType, 'utf8', 'hex');
         encryptedPhrase += cipher.final('hex');
-        shell.exec(`git checkout master`, {silent});
-        shell.exec(`echo ${encryptedPhrase} > ${requestType}.req`, {silent});
-        shell.exec(`git add ${requestType}.req`, {silent});
-        shell.exec(`git commit -m 'add request file'`, {silent});
-        shell.exec(`git push https://${username}:${gitToken}@github.com/${repo} master`, {silent});
+
+        let octokit = new Octokit({
+            auth: "token " + gitToken
+        });
+        await octokit.repos.createOrUpdateFile({
+            owner: username,
+            repo,
+            path: `${requestType}.req`,
+            branch: "master",
+            message: "add request file",
+            content: Buffer.from(encryptedPhrase).toString('base64'),
+            gitToken
+        });
         return true
     } catch (err) {
         throw err
@@ -21,13 +29,37 @@ async function encryptAndPutAuthFile(username, repo, algorithm, gitToken, reques
 
 async function removeAuthFiles(username, repo, gitToken, silent) {
     try {
-        shell.exec(`git checkout master`, {silent});
-        shell.exec(`rm add-cube.req`, {silent});
-        shell.exec(`rm add-cube-init.req`, {silent});
-        shell.exec(`git add add-cube.req`, {silent});
-        shell.exec(`git add add-cube-init.req`, {silent});
-        shell.exec(`git commit -m 'remove request files'`, {silent});
-        shell.exec(`git push https://${username}:${gitToken}@github.com/${repo} master`, {silent});
+        let octokit = new Octokit({
+            auth: "token " + gitToken
+        });
+
+        let sha1 = (await octokit.repos.getContents({
+            owner: username,
+            repo,
+            path: `add-cube.req`,
+        })).data.sha
+        await octokit.repos.deleteFile({
+            owner: username,
+            repo,
+            path: `add-cube.req`,
+            branch: "master",
+            message: "remove request file",
+            sha: sha1
+        })
+
+        let sha2 = (await octokit.repos.getContents({
+            owner: username,
+            repo,
+            path: `add-cube-init.req`,
+        })).data.sha
+        await octokit.repos.deleteFile({
+            owner: username,
+            repo,
+            path: `add-cube-init.req`,
+            branch: "master",
+            message: "remove request file",
+            sha: sha2
+        })
         return true
     } catch (err) {
         throw err
@@ -51,19 +83,19 @@ let addCube = async (username, cube, gitToken, repo) => {
         });
         if (res1.data.result) {
             // create add cube init request type file
-            await encryptAndPutAuthFile(username, repo,split('/')[1], algorithm, gitToken, "add-cube-init", _silent);
+            await encryptAndPutAuthFile(username, repo.split('/')[1], algorithm, gitToken, "add-cube-init", _silent);
 
-            let r =(await axios.post("https://0689c118.ngrok.io/api/add-cube-init", {
+            let r = (await axios.post("https://0689c118.ngrok.io/api/add-cube-init", {
                 username,
-                cube, 
+                cube,
                 gitToken,
                 repo: repo.split('/')[1]
             })).data;
-            
+
             if (r.result) {
                 await removeAuthFiles(username, repo.split('/')[1], gitToken, _silent)
             }
-            
+
             return r;
         }
     } catch (err) {
